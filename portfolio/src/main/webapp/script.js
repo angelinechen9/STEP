23057437af551ad5c3f1b7276f204fc858353a76
Angeline Chen <angelinechen9@gmail.com>
2020-06-22 20:42:33 +0000

    complete fetch walkthrough

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,3 +1,9 @@
+async function getData() {
+  const response = await fetch('/data');
+  const data = await response.text();
+  document.getElementById('data-container').innerHTML = data;
+}
+
 let i = 0;
 let text = "Learn to Code in HTML, CSS, JavaScript, jQuery, Node, Python, and Java";
 let speed = 100;
