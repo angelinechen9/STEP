77336dc2f7e793d6ba534969f805ef4d12bb283a
Angeline Chen <angelinechen9@gmail.com>
2020-06-22 17:36:37 +0000

    complete portfolio walkthrough

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,28 +1,20 @@
-// Copyright 2019 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+let i = 0;
+let text = "Learn to Code in HTML, CSS, JavaScript, jQuery, Node, Python, and Java";
+let speed = 100;
 
-/**
- * Adds a random greeting to the page.
- */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
-
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
-
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+function type() {
+	if (i < text.length) {
+		document.getElementById("text").innerHTML += text.charAt(i);
+		i++;
+		setTimeout(type, speed);
+	}
+	else {
+		document.getElementById("text").innerHTML = "";
+		i = 0;
+		type();
+	}
 }
+
+$(document).ready(function() {
+    type();
+})
