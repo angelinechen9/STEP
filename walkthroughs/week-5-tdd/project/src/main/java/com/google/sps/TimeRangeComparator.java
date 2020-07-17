6033e385459e3bc8ad3f0da4c4940ed39f0d5510
Angeline Chen <angelinechen9@gmail.com>
2020-07-17 22:49:59 +0000

    comment code

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/TimeRangeComparator.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/TimeRangeComparator.java
@@ -2,6 +2,9 @@ package com.google.sps;
 
 import java.util.Comparator;
 
+/**
+ * A comparator for sorting ranges by their start time in ascending order.
+ */
 public final class TimeRangeComparator implements Comparator<TimeRange> {
     public int compare(TimeRange a, TimeRange b) {
         return Long.compare(a.start(), b.start());
